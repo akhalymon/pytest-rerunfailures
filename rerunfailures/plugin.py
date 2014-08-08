7b20d50c50f4c072776b713dff81b43e33aff87c
Andrew Khalymon <akhalymon@hortonworks.com>
2014-08-08 18:12:49 +0300

    Added comments, reformatted code

--- rerunfailures/plugin.py
+++ rerunfailures/plugin.py
@@ -1,18 +1,17 @@
-import sys, time
 from _pytest.terminal import TerminalReporter
 import py, pytest
 
 from _pytest.runner import runtestprotocol
 
-# command line options
+# Add command line options
 def pytest_addoption(parser):
     group = parser.getgroup("rerunfailures", "re-run failing tests to eliminate flaky failures")
     group._addoption('--reruns',
-        action="store",
-        dest="reruns",
-        type="int",
-        default=0,
-        help="number of times to re-run failed tests. defaults to 0.")
+                     action="store",
+                     dest="reruns",
+                     type="int",
+                     default=0,
+                     help="number of times to re-run failed tests. defaults to 0.")
 
     group._addoption('--timelimit',
                      action="store",
@@ -38,6 +37,7 @@ def pytest_addoption(parser):
                      default=0,
                      help="Rerun tests after whole test suite finishes")
 
+
 @pytest.mark.trylast
 def pytest_configure(config):
     if hasattr(config, 'slaveinput'):
@@ -51,35 +51,45 @@ def pytest_configure(config):
         config.pluginmanager.unregister(standard_reporter)
         config.pluginmanager.register(reruninfo_reporter, 'terminalreporter')
     else:
+        # If no rerun option, completely unload plugin
         rerun_plugin = config.pluginmanager.getplugin('rerunfailures')
         config.pluginmanager.unregister(rerun_plugin)
+
 
 # making sure the options make sense
 # should run before / at the begining of pytest_cmdline_main
 def check_options(config):
+    # TODO add assertations for other options
     val = config.getvalue
     if not val("collectonly"):
         if config.option.reruns != 0:
-            if config.option.usepdb:   # a core option
+            if config.option.usepdb:  # a core option
                 raise pytest.UsageError("--reruns incompatible with --pdb")
+
 
 def pytest_sessionstart(session):
     # Initialising rerun time profiler
     session.ordinary_tests_durations = 0
     session.rerun_tests_durations = 0
 
+
+# This mark means hook will be called before default hook
+# (for reporting to use reduced number of tests, without re-started ones)
 @pytest.mark.tryfirst
 def pytest_sessionfinish(session, exitstatus):
+    # Removing duplicate items, leaving only the very last instance of each test
     items = session.items
     for item in items:
         while items.count(item) > 1:
             items.remove(item)
 
+# Init all elements to have attempt field
 def pytest_collection_modifyitems(session, config, items):
     """ called after collection has been performed, may filter or re-order
     the items in-place."""
     for item in items:
         item.attempt = 1
+
 
 def pytest_runtest_protocol(item, nextitem):
     """
@@ -92,33 +102,43 @@ def pytest_runtest_protocol(item, nextitem):
     item.ihook.pytest_runtest_logstart(
         nodeid=item.nodeid, location=item.location,
     )
-
+    # Do test execution
     reports = runtestprotocol(item, nextitem=nextitem, log=False)
+    # Update cumulative test durations
     update_test_durations(reports, item.session, item.attempt)
+    # Get test status (aware of rerun)
     test_succeed, status_message = report_test_status(item, reports)
+
+    if item.config.option.verbose:
+        print item.nodeid, " attepmt " + str(item.attempt)
+
     qualify_rerun = False
-    # print item.nodeid, " attepmt " + str(item.attempt)
     if test_succeed:
         pass
     else:
+        # Check rerun conditions
         qualify, reason = qualify_for_rerun(item, reports)
-        if not(qualify):
-            print "rerun skipped, reason: "+reason+" testcase: " + item.nodeid
+        if not (qualify):
+            print "rerun skipped, reason: " + reason + " testcase: " + item.nodeid
         else:
+            # Schedule item to be executed somewhere in future
             schedule_item_rerun(item, item.config)
             qualify_rerun = True
 
+    # Update report attempt field (to report these values)
     for report in reports:
+        # Only update for "call" (not setup and teardown)
         if report.when in ("call"):
             report.attempt = item.attempt
+        # If test is scheduled for rerun, results are not final, so we don't generate report
         if not qualify_rerun:
             item.ihook.pytest_runtest_logreport(report=report)
-
+        # For debug puproses
         verbose_output(item)
-
 
     # pytest_runtest_protocol returns True
     return True
+
 
 def verbose_output(item):
     if item.config.option.verbose:
@@ -126,7 +146,7 @@ def verbose_output(item):
         print "\n    time spent on runs: ", item.session.ordinary_tests_durations
         print "    time spent on reruns: \n", item.session.rerun_tests_durations
 
-
+# Get test execution results
 def report_test_status(item, reports):
     is_rerun = item.attempt > 1
     status_message = []
@@ -142,15 +162,18 @@ def report_test_status(item, reports):
         status_message.append("FAIL_ON_RERUN: " + item.nodeid)
     return test_succeed, "".join(status_message)
 
+# Depending on option, schedule rerun just after this item, or at the run end
 def schedule_item_rerun(item, config):
     item.attempt += 1
     if config.option.rerun_after:
         item.session.items.append(item)
     else:
-        item.session.items.insert(item.session.items.index(item)+1, item)
+        item.session.items.insert(item.session.items.index(item) + 1, item)
 
+# Decide if test is qulified for rerun
 def qualify_for_rerun(item, reports):
     reason = []
+    # Check if test is in [skipped tests]
     if item.config.option.skip_tests.split:
         tests_to_skip = [testname.strip() for testname in item.config.option.skip_tests.split(',')]
         for testname in tests_to_skip:
@@ -158,7 +181,8 @@ def qualify_for_rerun(item, reports):
                 reason.append("rerun explicitly disabled for this test case")
                 return False, "".join(reason)
 
-    if item.attempt > item.config.option.reruns+1:
+    # Check if there attempts for rerun left
+    if item.attempt > item.config.option.reruns + 1:
         reason.append("failure rerun attempt limit reached ")
         return False, "".join(reason)
 
@@ -169,26 +193,30 @@ def qualify_for_rerun(item, reports):
         return False, "".join(reason)
 
     # If overall rerun time exceeds threshold, skip
-    if item.session.rerun_tests_durations+get_test_duration(reports) > item.config.option.rerun_time_threshold:
+    if item.session.rerun_tests_durations + get_test_duration(reports) > item.config.option.rerun_time_threshold:
         reason.append("total rerun threshold reached")
         return False, "".join(reason)
 
+    # If qualify, reason is empty
     return True, "".join(reason)
+
 
 def update_test_durations(reports, session, attempt):
     current_test_duration = get_test_duration(reports)
     # If this is not a first try, add duration to reruns time, else to runs time
-    if attempt>1:
+    if attempt > 1:
         session.rerun_tests_durations += current_test_duration
     else:
         session.ordinary_tests_durations += current_test_duration
 
+
 def get_test_duration(reports):
     current_test_duration = 0
+    # reports is a list of stuff, executed for an item (setup, call, teardown)
+    # We count cumulative duration of it
     for j in range(len(reports)):
-         current_test_duration+= reports[j].duration
+        current_test_duration += reports[j].duration
     return current_test_duration
-
 
 
 def pytest_report_teststatus(report):
@@ -202,8 +230,8 @@ def pytest_report_teststatus(report):
             if report.outcome == "passed":
                 return "rerun passed", "R", "PASSED_ON_RERUN"
 
-
-
+# Adopted from https://github.com/jpvanhal/pytest-instafail/blob/master/pytest_instafail.py
+# With slight changes
 class RerunInfoTerminalReporter(TerminalReporter):
     def __init__(self, reporter):
         TerminalReporter.__init__(self, reporter.config)
@@ -219,7 +247,7 @@ class RerunInfoTerminalReporter(TerminalReporter):
                 keys.append(key)
         parts = []
         for key in keys:
-            if key: # setup/teardown reports have an empty key, ignore them
+            if key:  # setup/teardown reports have an empty key, ignore them
                 val = self.stats.get(key, None)
                 if val:
                     parts.append("%d %s" % (len(val), key))
@@ -244,6 +272,7 @@ class RerunInfoTerminalReporter(TerminalReporter):
         if exitstatus in (0, 1, 2, 4):
             self.summary_errors()
             self.summary_failures()
+            # Added summary for reruns
             self.summary_rerun_failed()
             self.summary_rerun_passed()
             self.summary_hints()
